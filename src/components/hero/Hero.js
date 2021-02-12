d06d3969a611df763f4a8350d5b4b28e0b157e99
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-12 13:44:07 -0600

    mockup home page with components

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hero extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>Hero goes here</h1>
+            </>
+        )
+    }
+}
+
+
+export default Hero;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import Menu from './components/menu/Menu';
+import Menu from '../menu/Menu';
+import Hero from '../hero/Hero';
+import Services from '../services/Services';
+import Footer from '../footer/Footer';
 
 
 
@@ -7,7 +10,10 @@ class Home extends React.Component{
     render(){
         return(
             <>
-           
+           <Menu/>
+           <Hero/>
+           <Services/>
+           <Footer />
             </>
         )
     }
