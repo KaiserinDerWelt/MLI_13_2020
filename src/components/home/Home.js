bb1e0c2f8374a077e0ad398b1e3625515a39d065
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-12 12:47:07 -0600

    add resources

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Home extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>Component here</h1>
+            </>
+        )
+    }
+}
+
+export default Home;
