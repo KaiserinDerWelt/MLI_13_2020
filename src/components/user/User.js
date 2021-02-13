19eeb493b0b9c18f1d3c8f0c8a525726ae4dfa3f
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-13 10:55:14 -0600

    Finished Styled Profile Page

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Menusername from '../menu/Menusername';
+import Welcomeuser from  './welcomeprofile/Welcomeuser';
+import Checking from  './accountmanagement/Checking';
 import Savings from  './accountmanagement/Savings';
+import Creditcard from  './accountmanagement/Creditcard';
 
 import Footer from '../footer/Footer';
 
@@ -8,9 +11,14 @@ class User extends React.Component{
     render(){
         return(
             <>
-           <Menusername />
+             <Menusername />
+             <div className="heroblackfull">
+             <Welcomeuser />
+           <Checking />
            <Savings />
-           <Footer/>
+           <Creditcard/>
+             </div>
+             <Footer/>
             </>
         )
     }
