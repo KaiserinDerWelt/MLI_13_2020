5a5b2731f7c6f70a55507e85d4dd1edb2771673e
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-13 22:22:53 -0600

    rendered functionalities components

---
+++ src/components/functionalities/Transactionaccordion.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menusername from '../menu/Menusername';
+import Footer from '../footer/Footer';
+
+class Transactionaccordion extends React.Component{
+    render(){
+        return(
+            <>
+            <Menusername />
+            <Footer/>
+            </>
+        )
+    }
+}
+
+export default Transactionaccordion;
