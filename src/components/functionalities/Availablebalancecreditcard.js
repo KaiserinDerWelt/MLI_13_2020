7a771dea6dcafa9dddefe3b850209736b116fa56
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-16 15:29:32 -0600

    css for extract pages

--- src/components/functionalities/Availablebalancecreditcard.js
+++ src/components/functionalities/Availablebalancecreditcard.js
@@ -6,9 +6,9 @@ class Availablebalancecreditcard extends React.Component{
             <>
              <div className="container-fluid">
         <div className="account-content-wrapper-extract">
-          <h3 className="account-title-general-balance">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount-general-balance">$184.30</p>
-          <p className="account-amount-description-general-balance">Available Balance</p>
+          <h3 className="account-title-general-balance-credit">Argent Bank Credit Card (x8349)</h3>
+          <p className="account-amount-general-balance-credit">$184.30</p>
+          <p className="account-amount-description-general-balance-credit">Available Balance</p>
         </div>
       </div>
             </>
--- src/components/functionalities/Availablebalancesaving.js
+++ src/components/functionalities/Availablebalancesaving.js
@@ -6,9 +6,9 @@ class Availablebalancesaving extends React.Component{
             <>
              <div className="container-fluid">
         <div className="account-content-wrapper-extract">
-          <h3 className="account-title-general-balance">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount-general-balance">$10,928.42</p>
-          <p className="account-amount-description-general-balance">Available Balance</p>
+          <h3 className="account-title-general-balance-saving">Argent Bank Savings (x6712)</h3>
+          <p className="account-amount-general-balance-saving">$10,928.42</p>
+          <p className="account-amount-description-general-balance-saving">Available Balance</p>
         </div>
       </div>
             </>
