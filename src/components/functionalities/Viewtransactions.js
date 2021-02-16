0e53e18e691952ebc883c4aef6f8d540a56041f5
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-16 09:52:11 -0600

    added server files

--- src/components/functionalities/Viewtransactions.js
+++ src/components/functionalities/Viewtransactions.js
@@ -13,36 +13,34 @@ class Viewtransactions extends React.Component{
         <th scope="col">Heading</th>
         <th scope="col">Heading</th>
         <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody> 
       <tr className="accordion-toggle collapsed" id="accordion1" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne">
 <td className="expand-button"></td>
 <td>Cell</td>
 <td>Cell</td>
 <td>Cell</td>
-
+<td>Add</td>
 </tr>
 <tr className="hide-table-padding">
 <td></td>
 <td colSpan="3">
 <div id="collapseOne" className="collapse in p-3">
   <div className="row">
-    <div className="col-2">label</div>
-    <div className="col-6">value 1</div>
+    <div className="col-2">Transaction Type:</div>
   </div>
   <div className="row">
-    <div className="col-2">label</div>
-    <div className="col-6">value 2</div>
+    <div className="col-2"></div>
   </div>
   <div className="row">
-    <div className="col-2">label</div>
-    <div className="col-6">value 3</div>
+    <div className="col-2"></div>
   </div>
   <div className="row">
-    <div className="col-2">label</div>
-    <div className="col-6">value 4</div>
+    <div className="col-2"></div>
   </div>
+
 </div></td>
 </tr>
       <tr className="accordion-toggle collapsed" id="accordion2" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
@@ -50,30 +48,27 @@ class Viewtransactions extends React.Component{
 <td>Cell</td>
 <td>Cell</td>
 <td>Cell</td>
-
+<td>Add</td>
 </tr>
 <tr className="hide-table-padding">
 <td></td>
 <td colSpan="4">
 <div id="collapseTwo" className="collapse in p-3">
   <div className="row">
-    <div className="col-2">label</div>
-    <div className="col-6">value</div>
+    <div className="col-2">Transaction Type</div>
   </div>
   <div className="row">
     <div className="col-2">label</div>
-    <div className="col-6">value</div>
   </div>
   <div className="row">
     <div className="col-2">label</div>
-    <div className="col-6">value</div>
   </div>
   <div className="row">
     <div className="col-2">label</div>
-    <div className="col-6">value</div>
   </div>
 </div></td>
 </tr>
+
     </tbody>
   </table>
 </div>
