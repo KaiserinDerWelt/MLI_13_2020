fe026d318e0faa72d2fa2303c9f1789d762f4a57
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-17 06:16:49 -0600

    Populated DB Succesfully

--- server/scripts/populateDatabase.js
+++ server/scripts/populateDatabase.js
@@ -3,15 +3,15 @@ const signupApi = 'http://localhost:3001/api/v1/user/signup'
 
 const users = [
   {
-    firstName: 'Tony',
-    lastName: 'Stark',
-    email: 'tony@stark.com',
+    firstName: 'Maria',
+    lastName: 'Stanley',
+    email: 'maria@stanley.com',
     password: 'password123'
   },
   {
-    firstName: 'Steve',
-    lastName: 'Rogers',
-    email: 'steve@rogers.com',
+    firstName: 'Sthela',
+    lastName: 'Rooster',
+    email: 'sthela@roster.com',
     password: 'password456'
   }
 ]
